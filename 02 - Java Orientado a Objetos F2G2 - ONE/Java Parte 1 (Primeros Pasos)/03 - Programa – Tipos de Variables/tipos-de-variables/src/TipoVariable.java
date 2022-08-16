b9e4762a6c0b6864b5b4eb04fca708086b0ceebb
Axel Adán <82789056+AxelAdan@users.noreply.github.com>
2022-08-15 22:36:17 -0500

    Fundamentos básicos de Java

---
+++ 02 - Java Orientado a Objetos F2G2 - ONE/Java Parte 1 (Primeros Pasos)/03 - Programa – Tipos de Variables/tipos-de-variables/src/TipoVariable.java
@@ -0,0 +1,42 @@
+
+public class TipoVariable {
+
+	public static void main(String[] args) {
+		// Int
+		int edad = 17;
+		
+		System.out.println(edad);
+		
+		edad = edad + 5;
+		
+		System.out.println("Mi edad es de " + edad + " años.");
+		
+		// Double
+		double salario = 1250.56;		
+		double salarioMitad = salario / 2 ;
+		System.out.println("La mitad del salario es " + salarioMitad); 
+		
+		// Cast of numbers
+		double numeroUno = 230.89;
+		int numeroDos = (int) numeroUno;
+		
+		System.out.println(numeroDos);
+		
+		// Long
+		long numeroLargo = 122222222222L;
+		System.out.println(numeroLargo);
+		
+		// Short
+		short numeroCorto = 13555;
+		System.out.println(numeroCorto);
+		
+		// Byte
+		byte numeroPequenio = 15;
+		System.out.println(numeroPequenio);
+		
+		// Float
+		float numeroDecimalPequenio = 2.5F;
+		System.out.println(numeroDecimalPequenio);
+	}
+
+}

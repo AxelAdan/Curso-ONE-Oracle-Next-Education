6d8e7b5776a69c9b3b9e54313ad7a2db9a434fcb
Axel Adán <82789056+AxelAdan@users.noreply.github.com>
2022-09-06 11:41:52 -0500

    Carga de ‘Trabajando con Caracteres’

---
+++ 02 - Java Orientado a Objetos F2G2 - ONE/Java Parte 1 (Primeros Pasos)/04 Programa - Trabajando con Caracteres/java-primeros-pasos/src/Caracteres.java
@@ -0,0 +1,25 @@
+
+public class Caracteres {
+
+	public static void main(String[] args) {
+		// Char
+		char caracter = 'a';
+		
+		System.out.println(caracter);
+		
+		caracter = 65;
+		
+		System.out.println(caracter);
+		
+		caracter = 65 + 1;
+		
+		System.out.println(caracter);
+		
+		char segundoCaracter = (char) (caracter + 1);
+		System.out.println(segundoCaracter);
+		
+		String palabra = "Palabra de ejemplo";
+		System.out.println(palabra);
+	}
+
+}
---
+++ 02 - Java Orientado a Objetos F2G2 - ONE/Java Parte 1 (Primeros Pasos)/04 Programa - Trabajando con Caracteres/java-primeros-pasos/src/ValoresVariables.java
@@ -0,0 +1,15 @@
+
+public class ValoresVariables {
+
+	public static void main(String[] args) {
+		int numeroUno = 5;
+		int numeroDos = 9;
+		
+		System.out.println(numeroDos);
+		
+		numeroDos = numeroUno;
+		
+		System.out.println(numeroDos);
+	}
+
+}
